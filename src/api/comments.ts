6c2e028065627bf4383ce5e6a44bc1f2ab4cbfae
yunusemreuveyik <yunusemreuveyik@hotmail.com>
2025-07-10 10:23:13 +0300

    file location updates

--- src/api/comments.ts
+++ src/api/comments.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../context/consts";
+import { BASE_URL } from "../models/consts";
 
 export const fetchComments = async () => {
   const { data } = await axios.get(`${BASE_URL}/comments`);
